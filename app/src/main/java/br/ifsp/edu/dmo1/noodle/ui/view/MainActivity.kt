e6f80c7b800f2c48ac71236435e3f28a3eec65bd
guilhermefragiacomo <guilhermefragiacomo1@gmail.com>
2025-01-29 17:14:25 -0300

    first commit

---
+++ app/src/main/java/br/ifsp/edu/dmo1/noodle/ui/view/MainActivity.kt
@@ -0,0 +1,10 @@
+package br.ifsp.edu.dmo1.noodle.ui.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
